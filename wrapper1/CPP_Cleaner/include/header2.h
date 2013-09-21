d65b4d15af2531e11dfe4b0507eb802cec36593e
dima <dima@dima-desktop>
2013-09-22 00:25:45 +0600

    Add new utility files.

---
+++ wrapper1/CPP_Cleaner/include/header2.h
@@ -0,0 +1,9 @@
+// Begin header2.h.
+
+#include <error.h>
+
+#define v2 2
+
+int f2();
+
+// End   header2.h.
---
+++ wrapper1/CPP_Cleaner/include/header3.h
@@ -0,0 +1,7 @@
+// Begin header3.h.
+
+#define v3 3
+
+int f3();
+
+// End   header3.h.
